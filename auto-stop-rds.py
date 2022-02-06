159430520d4efaa20c76e344751de80f2637c859
ryo00123 <ten3mm8apppoe@gmail.com>
2022-02-06 20:46:40 +0900

    rds

---
+++ auto-stop-rds.py
@@ -0,0 +1,29 @@
+import boto3
+rds = boto3.client('rds')
+def get_tags_for_db(db):
+    """
+    get instance list
+    """
+    instance_arn = db['DBInstanceArn']
+    instance_tags = rds.list_tags_for_resource(ResourceName=instance_arn)
+    return instance_tags['TagList']
+
+def stop_db_instances(target_tag):
+    """
+    stop instances
+    """
+    try:
+        dbs = rds.describe_db_instances()
+    except Exception as e:
+        print(e)
+        return { 'status': 2 }
+    for db in dbs['DBInstances']:
+        db_tags = get_tags_for_db(db)
+        tag = next(iter(filter(lambda tag: tag['Key'] == target_tag and tag['Value'] == 'true', db_tags)), None)
+        if tag and db['DBInstanceStatus'] == 'available':
+            response = rds.stop_db_instance(DBInstanceIdentifier=db["DBInstanceIdentifier"])
+            print(db['DBInstanceIdentifier'])
+def lambda_handler(event, context):
+    target_tag = "autostop"
+    stop_db_instances(target_tag)
+    return { 'status': 0 }
